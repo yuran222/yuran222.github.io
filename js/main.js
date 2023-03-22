2583883e4e21805d387d95f9baa8e5074f3477fd
yuran222 <yuran123456@gmail.com>
2023-03-22 17:41:23 +0300

    my final project

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.querySelector(".form__auth-btn").onclick = function () {
+  const userPassFirst = document.querySelector("#password1").value;
+  const userPassSecond = document.querySelector("#password2").value;
+
+  if (userPassFirst == "") {
+    document.getElementById("password1").style = "border: 1px solid red";
+    document.getElementById("password2").style = "border: 1px solid red";
+  } else if (userPassFirst != userPassSecond) {
+    document.getElementById("password1").style = "border: 1px solid red";
+    document.getElementById("password2").style = "border: 1px solid red";
+    return false;
+  } else if (userPassFirst == userPassSecond) {
+    document.getElementById("password1").style = "border: 1px solid green";
+    document.getElementById("password2").style = "border: 1px solid green";
+    event.preventDefault() // чтобы было видно, что подкрашивается зелёным, но чтоб не обновляло сайт
+  }
+  return true;
+};
+
